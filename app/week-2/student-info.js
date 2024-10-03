a9dd4a3fb993e43fce57e66b742b69e5f381b9eb
Surafel Kassa <surafelkassa10@yahoo.com>
2024-10-03 17:40:30 -0600

    week2-changes

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Surafel Kassa</p>
+      <Link href="https://github.com/SurafelKassa/cprg306-assignments.git" target="_blank">
+        Visit My GitHub
+      </Link>
+    </div>
+  );
+}
