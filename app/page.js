c076e823f90610c76943756fd872e41b0703a4ba
Surafel Kassa <surafelkassa10@yahoo.com>
2024-10-07 14:36:48 -0600

    week3-changes

--- app/page.js
+++ app/page.js
@@ -1,12 +1,19 @@
 import Link from 'next/link';
 
+
 export default function Home() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <Link href="/week-2">
          Week 2 
-      </Link>
+        </Link><br></br>
+
+        <Link href="/week-3">
+         Week 3
+        </Link>
     </main>
   );
 }
+
+
